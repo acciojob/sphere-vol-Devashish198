4f6c64c8e7b175560e2479e44375157f5a3cdf25
Devashish198 <dev.ashish.shukla281977@gmail.com>
2025-06-05 21:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
     //Write your code here
-  
-} 
+	event.preventDefault();
+	let r=parseInt(document.getElementById("radius").value);
+	
+    let v=document.getElementById("volume");
+	if (!isNaN(r)&&r>0) {
+    let volume = (4 / 3) * Math.PI * r * r * r;
+    v.value = volume.toFixed(4); // Set the value of the input field
+  } else {
+    v.value = "Nan";
+  }
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
